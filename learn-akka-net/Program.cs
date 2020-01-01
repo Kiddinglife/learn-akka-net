0df91fb8ab279210d8e40d99975f573a2c18c52f
jakez <jakez@ir.com>
2020-01-01 11:08:56 +1100

    * .gitignore: init
    * learn-akka-net.sln:
    * Program.cs:
    * packages.config:
    * learn-akka-net.csproj:
    * AssemblyInfo.cs:

---
+++ learn-akka-net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learnakkanet
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
